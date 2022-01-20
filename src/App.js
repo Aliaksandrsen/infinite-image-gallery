4142db047fa7fe3d54297a2ddf9e4c6d03c5ffe1
Aliaksandr Senkevich <puskin1914@gmail.com>
2022-01-20 11:53:29 +0300

    add unsplash api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
 
+const accessKey = process.env.REACT_APP_UNSLASH_ACCESS_KEY;
+
 export const App = () => {
+  const [images, setImages] = useState([]);
+  console.log('App ~ data', images);
+
+  useEffect(() => {
+    const fetchFunction = async () => {
+      const res = await fetch(
+        `https://api.unsplash.com/photos/?client_id=${accessKey}`
+      );
+      const data = await res.json();
+
+      setImages(data);
+    };
+    fetchFunction();
+  }, []);
+
+  if (!accessKey) {
+    return (
+      <a href="https://unsplash.com/oauth/applications" className="error">
+        Required: Get Your Unsplash API Key First
+      </a>
+    );
+  }
+
   return (
     <div className="app">
       <h1>Unsplash Image Gallery!</h1>
@@ -13,9 +38,9 @@ export const App = () => {
       </form>
 
       <div className="image-grid">
-        {[...Array(100)].map((_, index) => (
+        {images.map((image, index) => (
           <div className="image" key={index}>
-            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
+            <img src={image.urls.regular} alt={image.alt_description} />
           </div>
         ))}
       </div>
